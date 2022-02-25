b38fc655ee757ac5bb1477b203b8575b3e35ab73
Rodion Kotov <rodion.kotov@internet.ru>
2022-02-25 09:46:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab1/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+
+
+    }
+}
